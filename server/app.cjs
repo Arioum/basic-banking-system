0adf4bc068d8b4451fba569e3efcbcf24a771782
arioum <amoghrao121@gmail.com>
2023-03-08 18:03:29 +0530

    added mongoose

--- server/app.cjs
+++ server/app.cjs
@@ -1,10 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const http = require("http");
 const cors = require("cors");
 
 const app = express();
-http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
@@ -16,16 +16,19 @@ app.get("/all-customers", (req, res) => {
   res.json({ body: "Hello World" + Date.now() });
 });
 
-if (port) app.listen(port);
+const server = http.createServer(app);
 
-// mongoose.connect(process.env.MONGODB_URL).then(() => {
-//   console.log("Mongodb connected");
-//   server.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
-// }).catch((err) => {
-//   console.log({ err });
-//   process.exit(1);
-// });
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
 
 module.exports = app;
