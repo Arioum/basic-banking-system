b657663ae7c01fef4a67e090bb8d072413474c29
arioum <amoghrao121@gmail.com>
2023-03-09 12:29:19 +0530

    fix routes

--- server/app.cjs
+++ server/app.cjs
@@ -1,20 +1,18 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
 const http = require("http");
 const cors = require("cors");
+const routes = require("./routes/all.js");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 4000;
-
-app.get("/all-customers", (req, res) => {
-  res.json({ body: "Hello World" + Date.now() });
-});
 
 const server = http.createServer(app);
 
