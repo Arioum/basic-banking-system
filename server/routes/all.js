a0e67b45b514e362ed6a1bedbbfa2ff0a510a8d3
arioum <amoghrao121@gmail.com>
2023-03-09 16:53:04 +0530

    debug deploy 1

--- server/routes/all.js
+++ server/routes/all.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const getCustomers = require("../controllers/customer.controller");
+// const getCustomers = require("../controllers/customer.controller");
+const Customer = require("../models/customers");
 
 router.get("/all-customers", (req, res) => {
   res.json({ body: "Hello World" + Date.now() });
 });
 
-router.get("/customers", getCustomers);
+router.get("/customers", async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 router.get("/transactions", (req, res) => {
   res.json({ body: "Hello World" + Date.now() });
