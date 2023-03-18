dd8c96b239d055b3409ce7e7e15152c170c05f30
arioum <amoghrao121@gmail.com>
2023-03-18 22:47:40 +0530

    transfer logic

--- server/controllers/transaction.controller.js
+++ server/controllers/transaction.controller.js
@@ -1,12 +1,38 @@
 const Transaction = require("../models/transactions");
+const Customer = require("../models/customers");
 
 const createTransactions = async (req, res) => {
+  const { from, to, amount } = req.body;
+  const transaction = new Transaction({
+    from,
+    to,
+    amount,
+  });
+
   try {
-    const customers = await Customer.find();
-    res.status(200).json(customers);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+    await Customer.find({ email: transaction.from }).then(
+      async (fromResult) => {
+        await Customer.find({ email: transaction.to }).then((toResult) => {
+          if (fromResult.balance > amount)
+            return fromResult.balance - amount && toResult + amount;
+          else res.status(302).json({ message: "Insufficient balance" });
+        });
+      }
+    );
+  } catch (err) {}
+
+  transaction
+    .save()
+    .then((result) => {
+      console.log("Saved transaction", result);
+      res.status(201).json({
+        message: "Transfer request success",
+        createdTransaction: result,
+      });
+    })
+    .catch((error) => {
+      res.status(404).json({ message: error.message });
+    });
 };
 
 const getTransactions = async (req, res) => {
